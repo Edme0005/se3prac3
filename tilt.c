793ab0f4785417c2ab2fbd8a4e3c62f17303970e
David Edmeades <edme0005@flinders.edu.au>
2014-03-27 15:09:40 +1030

    Fixed many spelling mistakes, commented out combining loop, established shift left function

--- tilt.c
+++ tilt.c
@@ -7,17 +7,26 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
-  
+  int next = 0;
+  for (int i = 0; i < length; i++) {
+  	if (line[i] != 0) {
+  		line[next] = line[i];
+  		line[i] = 0;
+  		next++;
+  	}
+  }
 
+	/*
   // combine tiles as required
   for (int i = 0; i < length; i++) {
-  	if (lane[i] == lane[i+1]) {
+  	if (line[i] == line[i+1]) {
   		
   		//slide values down
-  		for (int j = i+1; j < length | lane[j+1] != 0; j++) {
-  			lane[j] = lane[j+1]
+  		for (int j = i+1; j < length | line[j+1] != 0; j++) {
+  			line[j] = line[j+1]
   		}
   	}
   }
+  */
   return 0;
 }
