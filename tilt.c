792205835e448ad0d881b0e489e8881522e0678f
David Edmeades <edme0005@flinders.edu.au>
2014-03-27 15:00:31 +1030

    Started loop for combining tile

--- tilt.c
+++ tilt.c
@@ -7,8 +7,17 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
+  
 
   // combine tiles as required
-
+  for (int i = 0; i < length; i++) {
+  	if (lane[i] == lane[i+1]) {
+  		
+  		//slide values down
+  		for (int j = i+1; j < length | lane[j+1] != 0; j++) {
+  			lane[j] = lane[j+1]
+  		}
+  	}
+  }
   return 0;
 }
