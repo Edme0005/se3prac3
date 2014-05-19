83cdedaccbaf7779c404d81c73c5e5a5d6ec12bb
David Edmeades <edme0005@flinders.edu.au>
2014-05-19 11:46:34 +0930

    Changed tilt right, now a copy of tilt left with loops changed.

--- tilt.c
+++ tilt.c
@@ -37,12 +37,34 @@ int tilt_line_left(int length,int *line)
 }
 
 int tilt_line_right(int length,int *line) {
-	int lineRev[4];
-	int i;
-	for(i = 0; i < length; i++) {
-		lineRev[i] = line[4-i];
-	}
-	tilt_line_left(length, lineRev);
-	line = lineRev;
+	if (length<1||length>255) return -1;
+
+  // slide tiles to the left
+  int next = length-1;
+  int i;
+  for (i = length-1; i >= 0; i--) {
+  	if (line[i] != 0) {
+  		if ( i != next) {
+  			line[next] = line[i];
+  			line[i] = 0;
+  		}
+  		next--;
+  	}
+  }
+  
+  // combine tiles as required
+  for (i = length-1; i >= 0; i--) {
+  	if (line[i] == line[i-1] && line[i] != 0) {
+  		line[i] += line[i];
+  		//slide values down
+  		int j;
+  		for (j = i-1; j >= 0; j--) {
+  			line[j] = line[j-1];
+  		}
+  		line[j] = 0;
+  	}
+  }
+  line[i] = 0;
+	
 	return 0;
 }
